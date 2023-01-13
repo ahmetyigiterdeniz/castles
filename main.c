c9db31f87d988d539b0c947362371992c72e612e
Erdeniz <108977954+erdenizw@users.noreply.github.com>
2023-01-13 15:42:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
 int main() {
-  int board[8][8], i, j;
+  char board[8][8], i, j;
         for(i = 0; i < 8; i++){
                 for(j = 0; j < 8; j++){
-                        board[i][j] = i*8+j+1;
+                        
+                        if(i == 1 || i == 6) {
+                                board[i][j] = 'P';
+                        }
+                        else if(j == 0 || j == 7) {
+                                board[i][j] = 'K';
+                        }
+                        else if(j == 1 || j == 6) {
+                                board[i][j] = 'A';
+                        } 
+                        else if(j == 2 || j == 5) {
+                                board[i][j] = 'F';
+                        }
+                        else if(j == 3) {
+                                board[i][j] = 'V';
+                        }
                 }
         }
   for(i = 0; i < 8; i++){
           for(j = 0; j < 8; j++){
-                  printf("[\t%d\t]", board[i][j]);
+                  printf("[ %c ]\t ", board[i][j]);
                   if(j == 7) {
                           printf("\n");
                   }
