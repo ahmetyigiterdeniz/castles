89f5f5722d73d323b106c72909b1e7ba195217f7
Erdeniz <108977954+erdenizw@users.noreply.github.com>
2023-01-19 10:48:58 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,17 +8,25 @@ int main() {
                         if(i == 1 || i == 6) {
                                 board[i][j] = 'P';
                         }
-                        else if(j == 0 || j == 7) {
+                        else if(i == 0 || i == 7) {
+                            if(j == 0 || j == 7) {
                                 board[i][j] = 'K';
-                        }
-                        else if(j == 1 || j == 6) {
+                            }
+                            else if(j == 1 || j == 6) {
                                 board[i][j] = 'A';
-                        } 
-                        else if(j == 2 || j == 5) {
+                            } 
+                            else if(j == 2 || j == 5) {
                                 board[i][j] = 'F';
-                        }
-                        else if(j == 3) {
+                            }
+                            else if(j == 3) {
                                 board[i][j] = 'V';
+                            }
+                            else if(j == 4) {
+                                board[i][j] = 'S';
+                            }
+                        }
+                        else {
+                            board[i][j] = ' ';
                         }
                 }
         }
